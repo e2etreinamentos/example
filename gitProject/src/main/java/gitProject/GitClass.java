897df43da17ae18035168ee7fec69275d97f2ed2
e2etreinamentos <58402584+e2etreinamentos@users.noreply.github.com>
2020-03-24 16:08:38 -0300

    Upload de projeto
    Primeira class java

---
+++ gitProject/src/main/java/gitProject/GitClass.java
@@ -0,0 +1,12 @@
+package gitProject;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("primeiro projeto no git");
+
+	}
+
+}
